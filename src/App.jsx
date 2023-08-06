22e453ff66c953a73998f568ace3d49f63383da6
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-08-05 19:40:58 -0600

    feat: add NewInvoiceBtn component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { FiltersDropdown } from "./components/FiltersDropdown";
+import { NewInvoiceBtn } from "./components/NewInvoiceBtn";
 
 function App() {
-  return <FiltersDropdown />;
+  return (
+    <>
+      <div className="flex items-center gap-4 tablet:gap-10">
+        <FiltersDropdown />
+        <NewInvoiceBtn />
+      </div>
+    </>
+  );
 }
 
 export default App;
