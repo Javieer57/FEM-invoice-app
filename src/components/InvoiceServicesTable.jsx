3d2d24b575411f1b9a88ca6394212c7d90e5fd66
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-08-06 16:19:27 -0600

    feat: details' table

---
+++ src/components/InvoiceServicesTable.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import { useCurrency } from "../hooks/useCurrency";
+
+export const ServicesTable = ({ invoice }) => {
+  return (
+    <table className="invoice-details-table">
+      <TableHead />
+      <TableBody items={invoice?.items} />
+      <TableFooter total={invoice?.total} />
+    </table>
+  );
+};
+
+const TableHead = () => {
+  return (
+    <thead>
+      <tr className="table-heading">
+        <th className="text-left">Item Name</th>
+        <th className="text-center">
+          <abbr title="quantity" className="no-underline">
+            QTY.
+          </abbr>
+        </th>
+        <th className="text-right">Price</th>
+        <th className="text-right">Total</th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = ({ items }) => {
+  const { formatToCurrency } = useCurrency();
+
+  return (
+    <tbody className="table-body">
+      {items.map((item, index) => (
+        <tr key={index}>
+          <td className="text-left">
+            {item?.name}
+            <br />
+            <span className="text-purple300 tablet:hidden">
+              {item?.quantity} x {formatToCurrency(item?.price)}
+            </span>
+          </td>
+          <td className="text-center">{item?.quantity}</td>
+          <td className="text-right">{formatToCurrency(item?.price)}</td>
+          <td className="text-right">{formatToCurrency(item?.total)}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+const TableFooter = ({ total }) => {
+  const { formatToCurrency } = useCurrency();
+  const [colPadding, setColPadding] = useState();
+
+  useEffect(() => {
+    setColPadding(window.innerWidth < 768 ? 1 : 3);
+
+    window.addEventListener("resize", () => {
+      setColPadding(window.innerWidth < 768 ? 1 : 3);
+    });
+  }, []);
+
+  return (
+    <tfoot className="table-footer">
+      <tr>
+        <td className="text-sm" colSpan={colPadding}>
+          Grand Total
+        </td>
+        <td className="text-right text-2xl font-bold">
+          {formatToCurrency(total)}
+        </td>
+      </tr>
+    </tfoot>
+  );
+};
--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,5 +1,70 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { StatusBadge } from "../components/StatusBadge";
+
+import { ServicesTable } from "../components/InvoiceServicesTable";
+
+const invoice = {
+  id: "RT3080",
+  createdAt: "2021-08-18",
+  paymentDue: "2021-08-19",
+  description: "Re-branding",
+  paymentTerms: 1,
+  clientName: "Jensen Huang",
+  clientEmail: "jensenh@mail.com",
+  status: "paid",
+  senderAddress: {
+    street: "19 Union Terrace",
+    city: "London",
+    postCode: "E1 3EZ",
+    country: "United Kingdom",
+  },
+  clientAddress: {
+    street: "106 Kendell Street",
+    city: "Sharrington",
+    postCode: "NR24 5WQ",
+    country: "United Kingdom",
+  },
+  items: [
+    {
+      name: "Brand Guidelines",
+      quantity: 1,
+      price: 1800.9,
+      total: 1800.9,
+    },
+  ],
+  total: 1800.9,
+};
 
 export const InvoicePage = () => {
-  return <div>InvoicePage</div>;
+  return (
+    <main className="mx-auto w-full max-w-[31.25rem] py-9 tablet:max-w-[42rem] tablet:py-16 desktop:max-w-[45.625rem] desktop:py-20">
+      <nav>
+        <Link
+          to="/"
+          className="inline-flex items-center gap-6 font-bold leading-none transition-colors hover:text-purple300 focus:text-purple300"
+        >
+          <img src="/assets/icon-arrow-left.svg" alt="" />
+          Go back
+        </Link>
+      </nav>
+
+      <header className="mb-4 rounded-lg bg-white p-6">
+        <div className="flex w-full items-center justify-between gap-3 font-medium text-gray200">
+          Status
+          <div>
+            <StatusBadge status="pending" />
+          </div>
+        </div>
+
+        {/* <div>edit delete mark as paid</div> */}
+      </header>
+
+      <section className="rounded-lg bg-white p-6">
+        {/* #XM9141 Graphic Design 19 Union Terrace London E1 3EZ United Kingdom
+        Invoice Date Bill To 21 Aug 2021 Alex Grim Payment Due 84 Church Way
+        Bradford BD1 9PB United Kingdom 20 Sep 2021 Sent to alexgrim@mail.com */}
+        <ServicesTable invoice={invoice} />
+      </section>
+    </main>
+  );
 };
