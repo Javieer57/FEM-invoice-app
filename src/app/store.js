b9e0fe5ef608bafee02442cb2a5ac42796d5efe0
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-08-12 14:24:37 -0600

    feat: remove ui from redux
    is not necessary to persist this states at the moment

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import uiReducer from "./slices/ui";
 import invoicesReducer from "./slices/invoices";
 
 export const store = configureStore({
   reducer: {
-    ui: uiReducer,
     invoices: invoicesReducer,
   },
 });
