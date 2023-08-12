6b2457eec2c580c9d85d1b17c42b9c27155707e3
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-08-12 13:46:04 -0600

    feat: add dynamic text for visible invoices

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,8 +1,11 @@
 import { FiltersDropdown } from "../components/FiltersDropdown";
 import { NewInvoiceBtn } from "../components/NewInvoiceBtn";
 import { InvoicesList } from "../components/InvoicesList";
+import { useInvoices } from "../hooks/useInvoices";
 
 export const Home = () => {
+  const { visibleInvoicesCopy } = useInvoices();
+
   return (
     <div className="mx-auto w-full max-w-[31.25rem] px-6 py-9 tablet:max-w-[42rem] tablet:px-0 tablet:py-16 desktop:max-w-[45.625rem] desktop:py-20">
       <header className="mb-8 flex items-center justify-between tablet:mb-14 desktop:mb-16">
@@ -11,8 +14,7 @@ export const Home = () => {
             Invoices
           </h1>
           <p className="text-sm font-medium text-gray200">
-            <span className="sr-only tablet:not-sr-only">There are</span> 4{" "}
-            <span className="sr-only tablet:not-sr-only">total</span> invoices
+            {visibleInvoicesCopy()}
           </p>
         </div>
 
