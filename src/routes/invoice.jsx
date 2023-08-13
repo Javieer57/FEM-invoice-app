d1f4ffa8051a6ad5ecdd4819333b2f5af4f177f1
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-08-13 16:40:19 -0600

    feat: handle no invoice error

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,34 +1,48 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useInvoices } from "../hooks/useInvoices";
 import {
   ServicesTable,
   InvoiceDetailsSummary,
   InvoiceHeader,
 } from "../components";
+import { useEffect } from "react";
 
 export const InvoicePage = () => {
   const { invoiceId } = useParams();
+  const navigate = useNavigate();
   const { getInvoiceById } = useInvoices();
   const invoice = getInvoiceById(invoiceId);
 
+  useEffect(() => {
+    if (!invoice) {
+      navigate("/");
+    }
+  }, [invoice]);
+
+  if (!invoice) {
+    return null;
+  }
+
   return (
-    <div className="mx-auto w-full max-w-[31.25rem] px-6 py-9 tablet:max-w-[42rem] tablet:px-0 tablet:py-16 desktop:max-w-[45.625rem] desktop:py-20">
-      <nav className="mb-8">
-        <GoBack />
-      </nav>
+    <>
+      <div className="mx-auto w-full max-w-[31.25rem] px-6 py-9 tablet:max-w-[42rem] tablet:px-0 tablet:py-16 desktop:max-w-[45.625rem] desktop:py-20">
+        <nav className="mb-8">
+          <GoBack />
+        </nav>
 
-      <header className="mb-4 rounded-lg bg-white p-6 shadow-light tablet:mb-6 tablet:flex tablet:items-center tablet:justify-between">
-        <InvoiceHeader invoice={invoice} />
-      </header>
+        <header className="mb-4 rounded-lg bg-white p-6 shadow-light tablet:mb-6 tablet:flex tablet:items-center tablet:justify-between">
+          <InvoiceHeader invoice={invoice} />
+        </header>
 
-      <section className="mb-28 rounded-lg bg-white p-6 text-purple300 shadow-light">
-        <div className="mb-10 grid grid-cols-2 gap-8 font-medium tablet:grid-cols-4">
-          <InvoiceDetailsSummary invoice={invoice} />
-        </div>
+        <section className="mb-28 rounded-lg bg-white p-6 text-purple300 shadow-light">
+          <div className="mb-10 grid grid-cols-2 gap-8 font-medium tablet:grid-cols-4">
+            <InvoiceDetailsSummary invoice={invoice} />
+          </div>
 
-        <ServicesTable invoice={invoice} />
-      </section>
-    </div>
+          <ServicesTable invoice={invoice} />
+        </section>
+      </div>
+    </>
   );
 };
 
